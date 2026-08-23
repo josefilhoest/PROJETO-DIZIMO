ebdbae9673ff8c0d8858d7ddf1283a9e9e6c7658
josefilhoest <josefilhoelineide2236@hotmail.com>
2026-08-23 15:51:48 -0300

    corrige API de producao no frontend

--- app/src/api/api.js
+++ app/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-baseURL: "http://localhost:8080/api",
+  baseURL: "https://projeto-dizimo.onrender.com/api",
 });
 
 // Adiciona o token automaticamente em todas as requisições
